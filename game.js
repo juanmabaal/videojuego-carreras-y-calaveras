910392952ed3dc4f60e245d1d06c897d486ae482
juanmabaal <juanma.baal@gmail.com>
2023-05-01 19:01:07 -0500

    Refactor del mapa de juego

--- game.js
+++ game.js
@@ -27,15 +27,23 @@ function startGame() {
     game.font = elementSize + `px Verdana`;
     game.textAlign = ``;
 
-    const map = maps[0];
+    const map = maps[2];
     const mapRows = map.trim().split(`\n`)
     const mapRowsCols = mapRows.map(row => row.trim().split(``))
 
+    mapRowsCols.forEach((row, rowI) => {
+        row.forEach ((col, colI) => {
+            const emoji = emojis [col]
+            const posX = elementSize * colI * 0.99;
+            const posY = elementSize * (rowI + 1) * 0.99;
+            game.fillText(emoji,posX, posY)
+        })
+    });
     console.log({map, mapRows})
 
-    for (let row = 1; row <= 10; row++) {
+    /* for (let row = 1; row <= 10; row++) {
         for (let col = 1; col <= 10; col++) {
             game.fillText(emojis[mapRowsCols[row - 1][col - 1]], elementSize * (col-1) * 0.99, elementSize * row *0.99 );
         }
-    }
+    } */
 }
